5182e02235d23676a993bbad568b7690fc104acc
aytacbey <59627848+aytacbey@users.noreply.github.com>
2022-12-10 15:32:18 +0300

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -6,7 +6,7 @@
 import argparse
 
 def get_args():
-	"""
+    """
 		Description:
 		Parses arguments at command line.
 
@@ -15,13 +15,71 @@ def get_args():
 
 		Return:
 			args - the arguments parsed
-	"""
-	parser = argparse.ArgumentParser()
+    """
+    parser = argparse.ArgumentParser()
 
-	parser.add_argument('--mode', dest='mode', type=str, default='train')              # can be 'train' or 'test'
-	parser.add_argument('--actor_model', dest='actor_model', type=str, default='/home/aytac/Masaüstü/00_Projeler/CARLA/PythonAPI/examples/ppo_actor_10_2_83.pth')     # your actor model filename
-	parser.add_argument('--critic_model', dest='critic_model', type=str, default='/home/aytac/Masaüstü/00_Projeler/CARLA/PythonAPI/examples/ppo_critic_10_2_83.pth')   # your critic model filename
+    parser.add_argument('--mode', dest='mode', type=str, default='train')  # can be 'train' or 'test'
+    parser.add_argument('--actor_model', dest='actor_model', type=str, 
+                        default='actor_model.pth')  # your actor model filename
+    parser.add_argument('--critic_model', dest='critic_model', type=str, 
+                        default='critic_doel.pth')  # your critic model filename
 
-	args2 = parser.parse_args()
+    args_PPO = parser.parse_args()
 
-	return args2
+    # Define arguments that will be received and parsed for CARLA
+    argparser = argparse.ArgumentParser(
+        description='CARLA No Rendering Mode Visualizer')
+    argparser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        dest='debug',
+        help='print debug information')
+    argparser.add_argument(
+        '--host',
+        metavar='H',
+        default='127.0.0.1',
+        help='IP of the host server (default: 127.0.0.1)')
+    argparser.add_argument(
+        '-p', '--port',
+        metavar='P',
+        default=2000,
+        type=int,
+        help='TCP port to listen to (default: 2000)')
+    argparser.add_argument(
+        '--res',
+        metavar='WIDTHxHEIGHT',
+        default='1280x720',
+        help='window resolution (default: 1280x720)')
+    argparser.add_argument(
+        '--filter',
+        metavar='PATTERN',
+        default='vehicle.*',
+        help='actor filter (default: "vehicle.*")')
+    argparser.add_argument(
+        '--map',
+        metavar='TOWN',
+        default=None,
+        help='start a new episode at the given TOWN')
+    argparser.add_argument(
+        '--no-rendering',
+        action='store_true',
+        help='switch off server rendering')
+    argparser.add_argument(
+        '--show-triggers',
+        action='store_true',
+        help='show trigger boxes of traffic signs')
+    argparser.add_argument(
+        '--show-connections',
+        action='store_true',
+        help='show waypoint connections')
+    argparser.add_argument(
+        '--show-spawn-points',
+        action='store_true',
+        help='show recommended spawn points')
+    
+    # pygame.init()
+    # Parse arguments
+    args_env = argparser.parse_args()
+    args_env.description = argparser.description
+
+    return args_PPO, args_env, args_env.description
